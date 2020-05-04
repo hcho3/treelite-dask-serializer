7e032351c0cf57b2e401170453e25bc277abb14b
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-05-04 15:25:04 -0700

    Struct formatting in memoryview dump

--- treelite_dask_serializer/example.py
+++ treelite_dask_serializer/example.py
@@ -17,29 +17,22 @@ def print_bytes(s):
     if len(s) % 48 != 0:
         print()
 
-def print_byte_ndarray(x):
-    for i, e in enumerate(x):
-        if (i + 1) % 48 == 1:
-            print('    ', end='')
-        print(f'{e:02X}', end='')
-        if (i + 1) % 48 == 0:
-            print()
-        elif (i + 1) % 16 == 0:
-            print('  ', end='')
-        else:
-            print(' ', end='')
-    if len(x) % 48 != 0:
-        print()
-
 def test_round_trip(model):
     frames = [np.asarray(x) for x in get_frames(model)]
     print('Python buffer frames:')
     for frame_id, frame in enumerate(frames):
-        print(f'  * Frame {frame_id}: dtype {frame.dtype}, length {len(frame)}')
-        if frame.dtype == np.uint8:
-            print_byte_ndarray(frame)
-        else:
+        if getattr(frame.dtype, 'names', None) is None:
+            print(f'  * Frame {frame_id}: dtype {frame.dtype}, length {len(frame)}')
             print(f'    {repr(frame)}')
+        else:  # Node type
+            print(f'  * Frame {frame_id}: dtype Node, length {len(frame)}')
+            print('    (cleft_, cright_, sindex_, info_, data_count_, sum_hess_, gain_, ' +
+                  'split_type_, cmp_, missing_category_to_zero_, data_count_present_, \n     ' +
+                  'sum_hess_present_, gain_present_)')
+            print('    array([')
+            for e in frame:
+                print(f'        {e}')
+            print('    ])')
     print()
 
     s = serialize(model)
